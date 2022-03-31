061a2adcd97687b632711d389aa132dee6d28ea1
BuddhiD-Workaholic <buddhi2002625@gmail.com>
2022-03-31 09:41:04 +0530

    Chnages been done

---
+++ Level.js
@@ -0,0 +1,34 @@
+class Levels {
+    constructor(JSONData, CLevel) {
+        this.CurentLevel = JSONData[CLevel - 1];
+        this.getOtherLevels(JSONData, CLevel);
+        console.log("Inside L: "+JSON.stringify(this.getCurentLevel(this.CurentLevel.xp)));
+    }
+
+    getOtherLevels(JSONData, CLevel) {
+        if (CLevel != 1) {
+            this.BelowLevel = JSONData[CLevel - 2];
+        } else {
+            this.BelowLevel = JSONData[CLevel - 1];
+        }
+        console.log("Below: " + JSON.stringify(this.BelowLevel));
+        console.log("Current: " + JSON.stringify(this.CurentLevel));
+        if (CLevel != 9) {
+            this.UpperLevel = JSONData[CLevel];
+        } else {
+            this.UpperLevel = JSONData[CLevel - 1];
+        }
+        console.log("Upper: " + JSON.stringify(this.UpperLevel));
+    }
+
+    getCurentLevel(score) {
+        if ((score => this.BelowLevel.xp) && (score < this.CurentLevel.xp)) {
+            return this.BelowLevel;
+        }
+        else if ((score => this.CurentLevel.xp) && (score < this.UpperLevel.xp)) {
+            return this.CurentLevel;
+        } else {
+            return this.UpperLevel;
+        }
+    }
+}
